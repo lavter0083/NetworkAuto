3dc52e6a69a80484f713f43a36fc59df26e4d046
lavter0083 <66638539+lavter0083@users.noreply.github.com>
2022-04-13 14:56:05 +0900

    Update history_newest.py

--- sample/history_newest.py
+++ sample/history_newest.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
         history_list = history(account)
 
         newest_cnt = min(5, len(history_list))
-        print "계정 : %s의 최근 사용 명령어 : % d개" % (account, newest_cnt)
+        print "acc : %sLast Use Commands : % d개" % (account, newest_cnt)
         if newest_cnt == 0:
-            print "\t기록된 이력 없음"
+            print "\tNothing use"
             
         i = 0
         while i < newest_cnt:

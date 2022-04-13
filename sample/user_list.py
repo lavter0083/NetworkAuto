fd2a1480fa2f3b2442ca60900e6b2bc803dc99b8
lavter0083 <66638539+lavter0083@users.noreply.github.com>
2022-04-13 14:28:04 +0900

    Update user_list.py

--- sample/user_list.py
+++ sample/user_list.py
@@ -1,37 +1,30 @@
-#!/bin/env python3
+#!/bin/env python
 #-*- coding: utf-8 -*-
 
 from subprocess import *
 
 def exec_cmd(cmd):
-    p= Popen(cmd, shell= True, stdout=PIPE, encoding= 'cp949')
-    # print(p)
-    (ret, err)= p.communicate()    
-    # print(ret, err)
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    (ret, err) = p.communicate()
     return ret
 
-def grep_login_defs(keyword):
-    ret= exec_cmd("grep '%s' /etc/login.defs" % keyword)
-    # print(ret)
+def grep_login_defs(keyword) :
+    # 사용자 계정 정보에서 필요한 값을 추출
+    ret = exec_cmd("grep '%s' /etc/login.defs" % keyword)
     return ret.split()[1]
-    
-def get_accounts():
-    min_u= grep_login_defs("^UID_MIN")
-    max_u= grep_login_defs("^UID_MAX")
-    
-    # print(min_u, max_u)
-    
-    cmd= ("awk -F':' -v 'min=%s'") % min_u
-    # print(cmd)
-    cmd= cmd + (" -v 'max=%s'") % max_u
-    # print(cmd)
-    cmd= cmd + (" '{ if ($3 >= min && $3 <= max) print $1}' /etc/passwd")
-    # print(cmd)
-    answer= exec_cmd(cmd).split()
-    # print(answer) 
-    return answer
 
-if __name__ == '__main__':
-    accouts= get_accounts()
-    for account in accouts:
-        print(account)
+def get_accounts() :
+    # grep_login_defs 함수로 일반 사용자 ID의 계정 범위 정보 추출
+    min_u = grep_login_defs("^UID_MIN")
+    max_u = grep_login_defs("^UID_MAX")
+
+    # 일반 사용자 계정 범위 정보를 조합한 명령어로 계정 리스트를 작성해서 반환
+    cmd = "awk -F':' -v 'min=%s'" % min_u
+    cmd = cmd + " -v 'max=%s'" % max_u
+    cmd = cmd + " '{ if ( $3 >= min && $3 <= max ) print $1}' /etc/passwd"
+    return exec_cmd(cmd).split()
+
+if __name__ == "__main__":
+    accounts = get_accounts()
+    for account in accounts :
+        print account
